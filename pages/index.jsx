b30d8ba44d6dec3c62957d914bf951e8fb7b6168
Vitor Hugo <vitor4oliveira@gmail.com>
2022-04-16 21:28:15 -0300

    Adding Presente component

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Presente from '../components/Presente'
+
+export default function Home() {
+  return (
+    <div>
+      <Presente />
+    </div>
+  )
+}
